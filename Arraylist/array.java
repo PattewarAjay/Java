c09c92103bcbae49bc65d88fe2c9c973aba4a13c
PattewarAjay <pattewarajay11@gmail.com>
2026-05-12 11:15:34 +0530

    3d array

--- Arraylist/array.java
+++ Arraylist/array.java
@@ -1,16 +1,21 @@
 public class array{
     public static void main(String[] args){
-        int[][] arr = new int[3][4];
+        int[][][] arr = new int[3][4][5];
 
         for(int i=0;i<3;i++){
             for(int j=0;j<4;j++){
-                arr[i][j] = (int)(Math.random()*10);
+                for(int k=0;k<5;k++){
+                    arr[i][j][k] = (int)(Math.random()*10);
+                }
             }
         }
 
         for(int i=0;i<3;i++){
             for(int j=0;j<4;j++){
-                System.out.print(arr[i][j]+" ");
+                for(int k=0;k<5;k++){
+                    System.out.print(arr[i][j][k]+" ");
+                }
+                System.out.println();
             }
             System.out.println();
         }
