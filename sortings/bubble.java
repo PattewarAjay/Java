0ee561c3fc28e5aee916ee8b977896153ff5feb4
PattewarAjay <pattewarajay11@gmail.com>
2026-05-15 16:45:50 +0530

    bubble sort

---
+++ sortings/bubble.java
@@ -0,0 +1,22 @@
+public class bubble{
+    public static void main(String[] args){
+        
+        int[] arr = {5,2,1,6,9,3};
+        int temp = 0;
+        int size = arr.length;
+        for(int i = 0; i<size; i++){
+            for(int j=0; j<size-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        
+        for(int nums : arr){
+            System.out.print(nums + " ");
+        }
+        
+    }
+}
