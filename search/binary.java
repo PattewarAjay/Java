69904d2af5fb47d44d1f5528140eacfd9aa025b4
PattewarAjay <pattewarajay11@gmail.com>
2026-05-15 16:18:15 +0530

    searches

---
+++ search/binary.java
@@ -0,0 +1,23 @@
+public class binary{
+    public static int binarysearch(int arr[], int target){
+        int left = 0;
+        int right = arr.length - 1;
+        while(left<right){
+            int mid=(left+right)/2;
+            if(arr[mid]==target){
+                return mid;
+            } else if(arr[mid]<target){
+                left = mid+1;
+
+            } else{
+                right = mid-1;
+            }
+        }
+        return -1;
+
+    }
+    public static void main(String[] args){
+        int arr[] = {2,3,5,6,8,9};
+        System.out.println(binarysearch( arr, 5));
+    }
+}
